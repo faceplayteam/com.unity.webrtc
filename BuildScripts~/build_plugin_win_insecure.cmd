704834ecce61d4d1d27ec9381db7cdfb1ff3c209
이승헌 <heonbert@nexon.co.kr>
2021-07-21 10:00:00 +0900

    Added a batch file to make custom package

---
+++ BuildScripts~/build_plugin_win_insecure.cmd
@@ -0,0 +1,17 @@
+@echo off
+
+set LIBWEBRTC_DOWNLOAD_URL=https://github.com/Unity-Technologies/com.unity.webrtc/releases/download/M89/webrtc-win.zip
+set SOLUTION_DIR=%cd%\Plugin~
+
+echo -------------------
+echo Download LibWebRTC 
+
+curl -L --insecure %LIBWEBRTC_DOWNLOAD_URL% > webrtc.zip
+7z x -aoa webrtc.zip -o%SOLUTION_DIR%\webrtc
+
+echo -------------------
+echo Build com.unity.webrtc Plugin
+
+cd %SOLUTION_DIR%
+cmake . -G "Visual Studio 16 2019" -A x64 -B "build64"
+cmake --build build64 --config Release --target WebRTCPlugin
---
+++ BuildScripts~/deploy_plugin_package.cmd
@@ -0,0 +1,37 @@
+@echo off
+
+rmdir /s /q .\Deploy
+
+mkdir Deploy\package
+cd Deploy\package
+
+mkdir Documentation~
+xcopy ..\..\Documentation~ Documentation~ /E
+
+mkdir Editor
+xcopy ..\..\Editor Editor /E
+
+mkdir Runtime
+xcopy ..\..\Runtime Runtime /E
+
+mkdir Samples~
+xcopy ..\..\Samples~ Samples~ /E
+
+mkdir Tests
+xcopy ..\..\Tests Tests /E
+
+copy ..\..\*.meta .\
+copy ..\..\*.md .\
+copy ..\..\*.json .\
+
+cd ..
+
+del /s /q *.pdb
+
+7z -ttar a dummy .\* -so | 7z -si -tgzip a com.faceplay.webrtc.tgz
+
+copy com.faceplay.webrtc.tgz ..\com.faceplay.webrtc.tgz
+
+cd ..
+
+rmdir /s /q .\Deploy
