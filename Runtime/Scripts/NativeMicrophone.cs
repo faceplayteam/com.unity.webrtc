4e9e2d5d9e51338ab182edfb86940e1758496c94
leejeonghun <jeonghun@outlook.com>
2021-12-09 10:38:57 +0900

    Add missing files

---
+++ Runtime/Scripts/NativeMicrophone.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Unity.WebRTC
+{
+    public class NativeMicrophone
+    {
+        public static string[] devices {
+            get => WebRTC.Context.GetMicrophoneDevices();
+        }
+
+        public static void SelectDevice(string deviceName)
+        {
+            if (!WebRTC.Context.SetMicrophone(deviceName))
+            {
+                throw new Exception("WebRTC.Context.SetMicrophone is failed.");
+            }
+        }
+    }
+}
